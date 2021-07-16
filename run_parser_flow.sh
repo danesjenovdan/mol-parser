ebc9a80e02c64efeb4cb8d51e9202224951305eb
Tomaz <tomazkunst@gmail.com>
2021-07-16 12:08:28 +0200

    update parser flow

--- run_parser_flow.sh
+++ run_parser_flow.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo "pasre sessions"
+echo "parse speeches, votes, questions"
 scrapy crawl sessions
 cd /app
-echo "set tfidf"
-python manage.py set_tfidf
 echo "start setting votes results"
 python manage.py set_votes_result --majority relative_normal
 echo "start setting motion tags"
 python manage.py set_motion_tags
 echo "start pairing votes with speeches"
 python manage.py pair_votes_and_speeches
+echo "lematize speeches"
+python manage.py lemmatize_speeches
+echo "set tfidf"
+python manage.py set_tfidf_for_sessions
+#TODO run analyses
 echo "send notifications"
 python manage.py send_daily_notifications
